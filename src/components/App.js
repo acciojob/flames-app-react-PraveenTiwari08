5d31f1e986ac5ccd19dc914c93a300eee3797415
PraveenTiwari08 <praviintiwari8@gmail.com>
2024-07-02 13:32:46 +0530

    second

--- src/components/App.js
+++ src/components/App.js
@@ -11,44 +11,60 @@ class App extends Component {
       result: "",
     };
 
-    this.handleinput1 = this.handleinput1.bind(this);
-    this.handleinput2 = this.handleinput2.bind(this);
-    this.Future = this.Future.bind(this);
+    this.handleInput1 = this.handleInput1.bind(this);
+    this.handleInput2 = this.handleInput2.bind(this);
+    this.calculateRelationship = this.calculateRelationship.bind(this);
     this.clear = this.clear.bind(this);
   }
 
-  handleinput1(event) {
+  handleInput1(event) {
     this.setState({ inputval1: event.target.value });
   }
 
-  handleinput2(event) {
+  handleInput2(event) {
     this.setState({ inputval2: event.target.value });
   }
 
-  Future() {
+  calculateRelationship() {
     const { inputval1, inputval2 } = this.state;
-    if(!inputval1 || !inputval2){
-        this.setState({result:"please enter valid input"})
-        return;
+    if (!inputval1 || !inputval2) {
+      this.setState({ result: "Please Enter valid input" });
+      return;
     }
+
     let str1 = inputval1.split("");
     let str2 = inputval2.split("");
 
-    str1 = str1.filter((char)=>{
-        const index = str2.indexOf(char);
-        if(index !== -1){
-            str1.splice(index,1);
-            return false;
-        }
-        return true;
+    // Create frequency maps for both strings
+    const freqMap1 = {};
+    const freqMap2 = {};
+
+    str1.forEach(char => {
+      freqMap1[char] = (freqMap1[char] || 0) + 1;
     });
 
-    const remaininglength = str1.length + str2.length;
-    const resultindex = remaininglength % 6;
+    str2.forEach(char => {
+      freqMap2[char] = (freqMap2[char] || 0) + 1;
+    });
+
+    // Remove common characters
+    for (let char in freqMap1) {
+      if (freqMap2[char]) {
+        const commonCount = Math.min(freqMap1[char], freqMap2[char]);
+        freqMap1[char] -= commonCount;
+        freqMap2[char] -= commonCount;
+      }
+    }
+
+    // Calculate remaining length
+    const remainingLength1 = Object.values(freqMap1).reduce((a, b) => a + b, 0);
+    const remainingLength2 = Object.values(freqMap2).reduce((a, b) => a + b, 0);
+    const remainingLength = remainingLength1 + remainingLength2;
+    const resultIndex = remainingLength % 6;
 
     const relationships = ["Siblings", "Friends", "Love", "Affection", "Marriage", "Enemy"];
-    
-    this.setState({ result:relationships[resultindex] });
+
+    this.setState({ result: relationships[resultIndex] });
   }
 
   clear() {
@@ -61,14 +77,17 @@ class App extends Component {
         <input
           data-testid="input1"
           value={this.state.inputval1}
-          onChange={this.handleinput1}
+          onChange={this.handleInput1}
         />
         <input
           data-testid="input2"
           value={this.state.inputval2}
-          onChange={this.handleinput2}
+          onChange={this.handleInput2}
         />
-        <button data-testid="calculate_relationship" onClick={this.Future}>
+        <button
+          data-testid="calculate_relationship"
+          onClick={this.calculateRelationship}
+        >
           C+R Future
         </button>
         <button data-testid="clear" onClick={this.clear}>
